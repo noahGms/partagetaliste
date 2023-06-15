2b4619638ef9da018ac6b879439972566ac79b25
noahGms <noah@keshin.net>
2023-06-15 18:51:00 +0200

    refactor again

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,75 +1,55 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: %i[ show edit update destroy ]
-  before_action :set_list, only: %i[ new create edit update destroy ]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_list, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
 
-  # GET /items or /items.json
-  def index
-    @items = Item.all
-  end
-
-  # GET /items/1 or /items/1.json
-  def show
-  end
-
-  # GET /items/new
   def new
     @item = Item.new
-    @item.list = @list
   end
 
-  # GET /items/1/edit
+  def create
+    @item = Item.new(item_params)
+    @item.list_id = @list.id
+
+    if @item.save
+      redirect_to list_url(@list), notice: "Item was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def edit
   end
 
-  # POST /items or /items.json
-  def create
-    @item = Item.new(item_params)
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to list_url(@list), notice: "Item was successfully created." }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /items/1 or /items/1.json
   def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to list_url(@list), notice: "Item was successfully updated." }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.update(item_params)
+      redirect_to list_url(@list), notice: "Item was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /items/1 or /items/1.json
   def destroy
     @item.destroy
 
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to list_url(@list), notice: "Item was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :category_id, :list_id, :username, :quantity)
+      params.require(:item).permit(:name, :category_id, :username, :quantity)
     end
+
     def set_list
       @list = List.find(params[:list_id])
     end
+
+  def authorize_user
+    redirect_to list_url(@list), alert: "You can't do that." unless @list.user == current_user
+  end
 end
